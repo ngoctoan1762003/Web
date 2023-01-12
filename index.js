4fad441585e2f7d02259096ee39f6397a22d4f4b
ngoctoan1762003 <54841155+ngoctoan1762003@users.noreply.github.com>
2023-01-12 13:52:19 +0700

    web

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-let result = prompt("What is ur name")
-if(result != null && result != ""){
-    document.getElementById("text").innerHTML='Hello ' + result
+const axios = require('axios')
+
+const fetchFoodData = () => {
+    axios.get('https://631b4048fae3df4dcff94f47.mockapi.io/api/foods').then(
+        (response) => {
+            const data = response.data
+            data.forEach((element, index) => {
+                if (element.name === 'nước táo')
+                    console.log(index + '. uống nước táo')
+                else if (element.name === 'pizza')
+                console.log(index + '. ăn pizza')
+            });
+        }
+    ).catch((error) => {
+        console.log("co loi");
+    })
 }
-else if(result == null || result == ""){
-    location.reload()
+
+const fetchData = () => {
+    axios.get('https://631b4048fae3df4dcff94f47.mockapi.io/api/todoItems').then(
+        (response) => {
+            const data = response.data
+            const todo = data.filter(data => data.status == 'todo')
+            console.log(todo)
+            const inProgress = data.filter(data => data.status == 'in-progress')
+            console.log(inProgress)
+            const done = data.filter(data => data.status == 'done')
+            console.log(done)
+        }
+    ).catch((error) => {
+        console.log("co loi");
+    })
 }
+
+//fetchFoodData()
+fetchData()
