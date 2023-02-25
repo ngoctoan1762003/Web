f7c9c4bbc14228eef1857be26e49cccd9479645c
ngoctoan1762003 <54841155+ngoctoan1762003@users.noreply.github.com>
2023-02-26 01:49:01 +0700

    web

--- EndExercise/index.js
+++ EndExercise/index.js
@@ -1,7 +1,56 @@
 let data
 let currentEdit
+let currentDrag
 let addWindow = document.querySelector('.addWindow')
 let editWindow = document.querySelector('.editWindow')
+let boxes = document.querySelectorAll('.box')
+
+boxes.forEach((box) => {
+    box.addEventListener('dragover', (e) => {
+        e.preventDefault()
+        box.classList.add('hovered')
+    })
+    box.addEventListener('dragleave', () => {
+        box.classList.remove('hovered')
+    })
+    box.addEventListener('drop', () => {
+        let index
+        for(let i=0; i<data.length; i++){
+            if(data[i].id == currentDrag){
+                index = i
+                break
+            }
+        }
+
+        if (box.classList.contains('todo-box')){
+            data[index].status = 'to-do'
+        }
+        if (box.classList.contains('doing-box')){
+            data[index].status = 'doing'
+        }
+        if (box.classList.contains('finished-box')){
+            data[index].status = 'finished'
+        }
+
+        axios.put(`https://63c96a17904f040a965db8df.mockapi.io/todo-list-2/${data[index].id}`, {
+            category: data[index].category,
+            title: data[index].title,
+            content: data[index].content,
+            status: data[index].status   
+        })
+
+        showItems()
+        box.classList.remove('hovered')
+    })
+})
+
+function showLoading() {
+    document.querySelector('.loading').style.display = 'flex'
+}
+
+function hideLoading() {
+    document.querySelector('.loading').style.display = 'none'
+}
 
 document.querySelector('.newTaskBtn').addEventListener('click', (event) => {
     event.preventDefault()
@@ -18,83 +67,101 @@ document.querySelector('.exit-edit-btn').addEventListener('click', (event) => {
     editWindow.style.display = "none";
 })
 
+const showItemsLeft = (todoItem, doingItem, finishedItem) => {
+    document.querySelector('.todo-item-left').innerHTML=todoItem
+    document.querySelector('.doing-item-left').innerHTML=doingItem
+    document.querySelector('.finished-item-left').innerHTML=finishedItem
+}
+
 const showItems = () => {
     let content = '';
     let contentDoing = '';
     let contentFinished = '';
+    let todoItem = 0
+    let doingItem = 0
+    let finishedItem = 0
 
     data.forEach((task) => {
         if(task.status == 'to-do'){
-            content += `<div class="card" id="${task.id}">
-                        <div class="cardHeader">
-                            <div class="info">
-                                <p class="category">${task.category}</p>
-                                <p class="title">${task.title}</p>
-                            </div>
-                            <div class="icon">
-                                <div class="editIcon flex">
-                                    <iconify-icon icon="teenyicons:edit-1-outline" class="w-[16px] h-[18px] cursor-pointer" onclick="edit(${task.id})"></iconify-icon>
-                                    <iconify-icon icon="iconoir:trash"  class="w-[24px] h-[25px] ml-5 cursor-pointer" onclick="del(${task.id})"></iconify-icon>
+            content += `<div class="card" id="${task.id}" draggable="true" ondragstart="dragStart(${task.id})">
+                            <div class="cardHeader">
+                                <div class="info">
+                                    <p class="category">${task.category}</p>
+                                    <p class="title">${task.title}</p>
+                                </div>
+                                <div class="icon">
+                                    <div class="editIcon flex">
+                                        <iconify-icon icon="teenyicons:edit-1-outline" class="w-[16px] h-[18px] cursor-pointer" onclick="edit(${task.id})"></iconify-icon>
+                                        <iconify-icon icon="iconoir:trash"  class="w-[24px] h-[25px] ml-5 cursor-pointer" onclick="del(${task.id})"></iconify-icon>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="h-0 w-[80%] border-2 border-solid border-[#DBDBDB] mt-[20px]"></div>
-                        <p class="content">${task.content}</p>
-                        <div class="timeBox flex gap-[7px] items-center">
-                            <iconify-icon icon="mdi:clock-time-nine-outline" flip="horizontal"></iconify-icon>
-                            <p class="time">June 30, 2022</p>
-                        </div>
-                    </div>`
+                            <div class="h-0 w-[80%] border-2 border-solid border-[#DBDBDB] mt-[20px]"></div>
+                            <p class="content">${task.content}</p>
+                            <div class="timeBox flex gap-[7px] items-center">
+                                <iconify-icon icon="mdi:clock-time-nine-outline" flip="horizontal"></iconify-icon>
+                                <p class="time">June 30, 2022</p>
+                            </div>
+                        </div>`
+            todoItem++
         }
         else if (task.status == 'doing'){
-            contentDoing += `<div class="card" id="${task.id}">
-            <div class="cardHeader">
-                <div class="info">
-                    <p class="category">${task.category}</p>
-                    <p class="title">${task.title}</p>
-                </div>
-                <div class="icon">
-                    <div class="editIcon flex">
-                        <iconify-icon icon="teenyicons:edit-1-outline" class="w-[16px] h-[18px] cursor-pointer" onclick="edit(${task.id})"></iconify-icon>
-                        <iconify-icon icon="iconoir:trash"  class="w-[24px] h-[25px] ml-5 cursor-pointer" onclick="del(${task.id})"></iconify-icon>
-                    </div>
-                </div>
-            </div>
-            <div class="h-0 w-[80%] border-2 border-solid border-[#DBDBDB] mt-[20px]"></div>
-            <p class="content">${task.content}</p>
-            <div class="timeBox flex gap-[7px] items-center">
-                <iconify-icon icon="mdi:clock-time-nine-outline" flip="horizontal"></iconify-icon>
-                <p class="time">June 30, 2022</p>
-            </div>
-        </div>`
+            contentDoing += `<div class="card" id="${task.id}" draggable="true" ondragstart="dragStart(${task.id})">
+                                <div class="cardHeader">
+                                    <div class="info">
+                                        <p class="category">${task.category}</p>
+                                        <p class="title">${task.title}</p>
+                                    </div>
+                                    <div class="icon">
+                                        <div class="editIcon flex">
+                                            <iconify-icon icon="teenyicons:edit-1-outline" class="w-[16px] h-[18px] cursor-pointer" onclick="edit(${task.id})"></iconify-icon>
+                                            <iconify-icon icon="iconoir:trash"  class="w-[24px] h-[25px] ml-5 cursor-pointer" onclick="del(${task.id})"></iconify-icon>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="h-0 w-[80%] border-2 border-solid border-[#DBDBDB] mt-[20px]"></div>
+                                <p class="content">${task.content}</p>
+                                <div class="timeBox flex gap-[7px] items-center">
+                                    <iconify-icon icon="mdi:clock-time-nine-outline" flip="horizontal"></iconify-icon>
+                                    <p class="time">June 30, 2022</p>
+                                </div>
+                            </div>`
+            doingItem++
         }
         else {
-            contentFinished += `<div class="card" id="${task.id}">
-            <div class="cardHeader">
-                <div class="info">
-                    <p class="category">${task.category}</p>
-                    <p class="title">${task.title}</p>
-                </div>
-                <div class="icon">
-                    <div class="editIcon flex">
-                        <iconify-icon icon="teenyicons:edit-1-outline" class="w-[16px] h-[18px] cursor-pointer" onclick="edit(${task.id})"></iconify-icon>
-                        <iconify-icon icon="iconoir:trash"  class="w-[24px] h-[25px] ml-5 cursor-pointer" onclick="del(${task.id})"></iconify-icon>
-                    </div>
-                </div>
-            </div>
-            <div class="h-0 w-[80%] border-2 border-solid border-[#DBDBDB] mt-[20px]"></div>
-            <p class="content">${task.content}</p>
-            <div class="timeBox flex gap-[7px] items-center">
-                <iconify-icon icon="mdi:clock-time-nine-outline" flip="horizontal"></iconify-icon>
-                <p class="time">June 30, 2022</p>
-            </div>
-        </div>`
+            contentFinished += `<div class="card" id="${task.id}" draggable="true" ondragstart="dragStart(${task.id})">
+                                    <div class="cardHeader">
+                                        <div class="info">
+                                            <p class="category">${task.category}</p>
+                                            <p class="title">${task.title}</p>
+                                        </div>
+                                        <div class="icon">
+                                            <div class="editIcon flex">
+                                                <iconify-icon icon="teenyicons:edit-1-outline" class="w-[16px] h-[18px] cursor-pointer" onclick="edit(${task.id})"></iconify-icon>
+                                                <iconify-icon icon="iconoir:trash"  class="w-[24px] h-[25px] ml-5 cursor-pointer" onclick="del(${task.id})"></iconify-icon>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="h-0 w-[80%] border-2 border-solid border-[#DBDBDB] mt-[20px]"></div>
+                                    <p class="content">${task.content}</p>
+                                    <div class="timeBox flex gap-[7px] items-center">
+                                        <iconify-icon icon="mdi:clock-time-nine-outline" flip="horizontal"></iconify-icon>
+                                        <p class="time">June 30, 2022</p>
+                                    </div>
+                                </div>`
+            finishedItem++
         }
     })
+
+    showItemsLeft(todoItem, doingItem, finishedItem)
 
     document.querySelector('.todo-box').innerHTML = content
     document.querySelector('.doing-box').innerHTML = contentDoing
     document.querySelector('.finished-box').innerHTML = contentFinished
+}
+
+function dragStart(id){
+    currentDrag = id
 }
 
 document.querySelector('.submit-btn').addEventListener('click', (e) => {
@@ -173,7 +240,7 @@ document.querySelector('.submit-edit-btn').addEventListener('click', (event) => 
 })
 
 async function fetchData() {
-    //showLoading()
+
     try {
         const response = await axios.get('https://63c96a17904f040a965db8df.mockapi.io/todo-list-2')
         console.log("oke")
@@ -182,15 +249,18 @@ async function fetchData() {
 
         showItems()
     } catch (e) {
-        alert("co loi");
+        alert("Khong the lay du lieu");
     }
 }
 
 async function main(){
+    showLoading()
     addWindow.style.display = "none";
     editWindow.style.display = "none";
     try{
-        await fetchData()
+        fetchData().then( () => {
+            hideLoading()   
+        })
     }
     catch (error) {
         alert ("Failed");
